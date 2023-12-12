d6004b70e70e4241e23e7367b040bdd9af8dc7bc
NisanurKorkmaz <nisanur29@live.fr>
2023-12-12 15:18:52 +0300

    ss

---
+++ Build/Intermediates.noindex/Previews/AirbnbClone/Intermediates.noindex/AirbnbClone.build/Debug-iphonesimulator/AirbnbClone.build/Objects-normal/x86_64/ListingView.1.preview-thunk.swift
@@ -0,0 +1,26 @@
+@_private(sourceFile: "ListingView.swift") import AirbnbClone
+import func SwiftUI.__designTimeBoolean
+import func SwiftUI.__designTimeInteger
+import protocol SwiftUI.PreviewProvider
+import func SwiftUI.__designTimeString
+import func SwiftUI.__designTimeFloat
+import struct SwiftUI.EmptyView
+import protocol SwiftUI.View
+import SwiftUI
+
+extension ListingView {
+    @_dynamicReplacement(for: body) private var __preview__body: some View {
+        #sourceLocation(file: "/Users/nisanurkorkmaz/Desktop/AirbnbClone/AirbnbClone/Core/Listings/ListingView.swift", line: 12)
+        Text(/*@START_MENU_TOKEN@*/__designTimeString("#2150.[1].[0].property.[0].[0].arg[0].value", fallback: "Hello, World!")/*@END_MENU_TOKEN@*/)
+    
+#sourceLocation()
+    }
+}
+
+import struct AirbnbClone.ListingView
+#Preview {
+    ListingView()
+}
+
+
+

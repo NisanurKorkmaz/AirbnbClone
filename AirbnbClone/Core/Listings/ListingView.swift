25ea4ac50fdad05e79e877ae5926f70c83ac150b
NisanurKorkmaz <nisanur29@live.fr>
2023-12-12 15:16:08 +0300

    s

---
+++ AirbnbClone/Core/Listings/ListingView.swift
@@ -0,0 +1,18 @@
+//
+//  ListingView.swift
+//  AirbnbClone
+//
+//  Created by Nisanur Korkmaz on 12/12/2023.
+//
+
+import SwiftUI
+
+struct ListingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListingView()
+}
